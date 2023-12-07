2af0eb67edc5368c295d7a2394ddae5185e523f8
Giovane Simamoto <60979790+GiSima@users.noreply.github.com>
2023-12-07 11:21:06 -0300

    Changes (Dependencies, formatting)

---
+++ dependency_api/src/main/java/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import buy.desafio.api.domain.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
